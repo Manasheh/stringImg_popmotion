9096aa363f503c9ea4fd10063185425feb441b95
Menashe vaiphei <vaipheimanasheh@gmail.com>
2023-02-26 22:41:40 +0200

    edit

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ listen(document, 'mouseup touchend')
 
 var nextContainer = document.querySelector('.second-container');
 var h = 900; // x vertex, half of total bounce duration
-var k = 220; // y vertex, total bounce height
-var a = -6 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
+var k = 260; // y vertex, total bounce height
+var a = -8 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
 var ypos, start, time;
 
 (function drawPosition(timestamp) {
@@ -46,8 +46,8 @@ var ypos, start, time;
 
 var thirdContainer = document.querySelector('.third-container');
 var h = 900; // x vertex, half of total bounce duration
-var k = 220; // y vertex, total bounce height
-var a = -6 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
+var k = 260; // y vertex, total bounce height
+var a = -8 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
 var ypos, start, time;
 
 (function drawPosition(timestamp) {
@@ -63,8 +63,8 @@ var ypos, start, time;
 
 var forthContainer = document.querySelector('.forth-container');
 var h = 900; // x vertex, half of total bounce duration
-var k = 220; // y vertex, total bounce height
-var a = -6 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
+var k = 260; // y vertex, total bounce height
+var a = -8 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
 var ypos, start, time;
 
 (function drawPosition(timestamp) {
@@ -80,8 +80,8 @@ var ypos, start, time;
 
 var fifthContainer = document.querySelector('.fifth-container');
 var h = 900; // x vertex, half of total bounce duration
-var k = 220; // y vertex, total bounce height
-var a = -6 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
+var k = 260; // y vertex, total bounce height
+var a = -8 * k / Math.pow(h * 2, 2); // coefficient: -.000483932
 var ypos, start, time;
 
 (function drawPosition(timestamp) {
